591f241665c88dc37ae3669fffc9855d494c02b3
Cuervolu <angel.cuervo187@gmail.com>
2024-08-15 00:58:36 -0400

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  vite:{
+    optimizeDeps: {
+      exclude: ['vee-validate']
+    }
+  },
+  modules: [
+    "nuxt-codemirror",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/eslint",
+    "@nuxtjs/mdc",
+    "@vee-validate/nuxt",
+    "@nuxt/fonts",
+    "@nuxtjs/seo",
+    "shadcn-nuxt"
+  ],
+  srcDir: 'src',
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: 'src/components/ui',
+  },
+  pinia: {
+    storesDirs: ['src/stores/**'],
+  },
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', {injectPosition: 'first'}],
+    configPath: 'tailwind.config.js',
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+})
